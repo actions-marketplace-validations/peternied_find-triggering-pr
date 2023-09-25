2dc6579673bdb524f671c6ee49aae5d229e2d461
Peter Nied <peternied@hotmail.com>
2023-09-25 09:25:13 -0500

    Initial release
    Signed-off-by: Peter Nied <peternied@hotmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# find-triggering-pr
+# Find Triggering Pull Request
 Finds the pull request associated with a triggered event
+
+
+```yaml
+inputs:
+  token:
+    description: "GitHub token used for authentication"
+    required: true
+
+outputs:
+  pr-number:
+    description: "The pull request number, if found, otherwise undefined"
+```
+
+## Usage:
+
+```yaml
+on:
+  workflow_run:
+    types: completed
+...
+jobs:
+  useful-job:
+    runs-on: ubuntu-latest
+    steps:
+      - id: find-triggering-pr
+        uses: peternied/find-triggering-pr@v1
+        with:
+          token: ${{ secrets.GITHUB_TOKEN }}
+
+      - run: echo ${{ steps.find-triggering-pr.outputs.pr-number }}
+```
+
+# Changelog
+
+## v1
+- Initial Release
---
+++ index.js
@@ -0,0 +1,29 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { Octokit } = require("@octokit/action");
+
+async function run() {
+  try {
+    const octokit = new Octokit();
+
+    const { context } = github;
+    const { repository } = context.payload;
+    const workflowRunId = context.payload.workflow_run.id;
+
+    core.info(`Checking workflow_run id ${workflowRunId}`);
+
+    const workflowRun = await github.rest.actions.getWorkflowRun({
+      owner: repository.owner.login,
+      repo: repository.name,
+      run_id: workflowRunId,
+    });
+
+    const result = { "pr-number": workflowRun.data?.pull_requests[0]?.number };
+    core.info(`Result ${result}`)
+    return result;
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
